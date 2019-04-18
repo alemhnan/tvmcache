352ed3c2c431cb46398033b3275dec042d08cd7c
Alessandro Maccagnan <alessandro.maccagnan@gmail.com>
2019-04-18 12:15:13 +0200

    add uptime in root

--- libs/Controllers/index.ts
+++ libs/Controllers/index.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import MongoDB from '../MongoDB';
 import { Show } from '../Interfaces';
+const started = new Date();
 
 const PAGE_SIZE = 250;
 
@@ -11,17 +12,21 @@ let getShows: (skip: number, limit: number) => Promise<Show[]>;
   getShows = mongoMethods.getShows;
 })();
 
-export let root = (req: Request, res: Response) => res.send('TV Maze Cache');
+export let root = (req: Request, res: Response) => {
+  const uptime = (Date.now() - Number(started)) / 1000;
+
+  return res.json({ uptime });
+};
 
 export let shows = async (req: Request, res: Response) => {
   const page: number = Math.floor(Number(req.query.page)) || 0;
 
   const skip = page * PAGE_SIZE;
   const limit = PAGE_SIZE;
+
   try {
     const shows = await getShows(skip, limit);
     return res.json(shows);
-
   } catch (error) {
     console.log(error);
     return res.json([]);
